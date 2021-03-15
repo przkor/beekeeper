dcb3e982a33a02df1b834cc2382e175c1faaef7f
Przemek Korona <przkor@gmail.com>
2021-03-15 11:32:37 +0100

    add MigrationHives.js and MigrationTable comp.

---
+++ src/components/Hives/NEW/MigrationHives.js
@@ -0,0 +1,46 @@
+import React, {useContext,useLayoutEffect} from 'react' ;
+import {Provider} from 'react-redux'
+import {store} from '../../../store/hivesStore.js'
+import {useHistory} from 'react-router-dom'
+import { ContextLogin } from '../../ContextLogin';
+import NotLoggedComponent from '../../NotLoggedComponent'
+import MigrationTable from './MigrationTable.js'
+
+const MigrationComponent = () => {
+  return (
+          <MigrationTable/>      
+  )
+}
+
+
+const Migration = () => {
+
+  const {isUserLogged} = useContext(ContextLogin) 
+  const history = useHistory()
+
+  useLayoutEffect(() => {
+    document.getElementById('showHives').className='nav-link';
+    document.getElementById('addHive').className='nav-link';
+    document.getElementById('migrationHives').className='nav-link active';
+  },[])
+ 
+  return (
+   <> 
+   {
+      isUserLogged 
+      ? 
+      <>
+      <Provider store={store}>
+        <div>
+          <MigrationComponent/>
+        </div>
+      </Provider>
+     </>
+     : 
+      <NotLoggedComponent history={history}/>
+   }
+   </>    
+  );
+}
+
+export default Migration;
