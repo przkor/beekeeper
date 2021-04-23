732972874fb364dbdfbfff7621b51c143119ada2
Przemek Korona <przkor@gmail.com>
2021-04-23 10:56:39 +0200

    delete inactive links

--- src/pages/Managers.js
+++ src/pages/Managers.js
@@ -3,7 +3,7 @@ import {Link,useHistory} from 'react-router-dom'
 import { ContextLogin } from '../components/ContextLogin'
 import NotLoggedComponent from  '../components/NotLoggedComponent'
 
-const menuList = ["pasieki","ule","odklady","matki","leczenie","akcje"]
+const menuList = ["pasieki","ule","matki","akcje"]
 const ManagerMenu = (props) => {
     const {isUserLogged} = useContext(ContextLogin)  
     const history = useHistory()
